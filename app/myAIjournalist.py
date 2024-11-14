2f97d543a456b6f7d1f5428335b8facf07b3c081
Raul Gauli <raulgauli@Rauls-MacBook-Pro.local>
2024-11-14 13:38:49 -0600

    add new AI file

--- app/myAIjournalist.py
+++ app/myAIjournalist.py
@@ -13,11 +13,24 @@ class AIJournalist:
     async def get_article_content_from_url(url):
         article = requests.get(url)
         article_content = article.text
-        return article_content
+        print(article_content)
+        # return article_content
 
     @staticmethod
     async def summarized_content(content):
-        summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+        print("lets summarize the content")
+        summarizer = pipeline("summarization", 
+                            model="facebook/bart-large-cnn", 
+                            framework="pt",
+                            device=0
+                            )
+        print(summarizer)
         content_from_url = await AIJournalist.get_article_content_from_url(content['url'])
-        response = (summarizer(content_from_url, max_length=130, min_length=30, do_sample=False))
-        return response[0]['summary_text']
+        chunks = [content_from_url[i:i+512] for i in range(0, len(content_from_url), 512)]
+        print(len(chunks))
+        summary = ""
+        for achunk in chunks:
+            response = (summarizer(achunk, max_length=130, min_length=30, do_sample=False))
+            print(response)
+            summary += response[0]["summary_text:"]
+        return summary
